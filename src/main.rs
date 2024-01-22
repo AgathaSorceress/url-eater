AgathaSorceress/url-eater

commit 4b0b4ef2489e9e8c0b9fef71ae0cf7635da6c6b1

Fix handling of URI fragments when a URI has no query component

src/main.rs
@@ -61,6 +61,11 @@ fn clean_url(text: String, patterns: Vec<String>) -> Result<String, String> {
         return Err(format!("URL {url_inner} does not have a host"));
     };
 
+    // Handle URLs without query parameters
+    if url.query().is_none() {
+        return Ok(url.to_string());
+    }
+
     for pattern in &patterns {
         let url_inner = url.clone();
         if let Some((param, domain)) = pattern.split_once('@') {
@@ -82,8 +87,5 @@ fn clean_url(text: String, patterns: Vec<String>) -> Result<String, String> {
         }
     }
 
-    // Handle dangling ?s when no query pairs are appended
-    let url = url.as_str().trim_end_matches('?').to_owned();
-
-    Ok(url)
+    Ok(url.to_string())
 }

commit 3ea3d1363d61654d489f31578994bcb799b683b2

Fix regression introduced in 4b0b4ef; add tests

.github/workflows/build_nix.yml
@@ -27,3 +27,5 @@ jobs:
       - uses: cachix/install-nix-action@v23
       - name: Lint
         run: nix develop --command cargo clippy --all-targets --all-features
+      - name: Test
+        run: nix develop --command cargo test

Cargo.toml
@@ -14,5 +14,8 @@ arboard = { version = "3.3.0", features = ["wayland-data-control"] }
 knuffel = "3.0.0"
 memoize = { version = "0.4.0", features = ["full"] }
 miette = { version = "5.7.0", features = ["fancy"] }
-url = "2.3.1"
+url = "2.5.0"
 wildmatch = "2.1.1"
+
+[dev-dependencies]
+pretty_assertions = "1.4.0"

src/main.rs
@@ -1,12 +1,15 @@
 use arboard::Clipboard;
 use memoize::memoize;
 use miette::{miette, Result};
-use std::{env, time::Duration};
+use std::{borrow::Cow, env, time::Duration};
 use url::Url;
 use wildmatch::WildMatch;
 
 mod config;
 use config::Config;
+
+#[cfg(test)]
+mod tests;
 
 /// How often should clipboard be checked for changes (0 will result in high CPU usage)
 const ITERATION_DELAY: Duration = Duration::from_millis(250);
@@ -61,11 +64,6 @@ fn clean_url(text: String, patterns: Vec<String>) -> Result<String, String> {
         return Err(format!("URL {url_inner} does not have a host"));
     };
 
-    // Handle URLs without query parameters
-    if url.query().is_none() {
-        return Ok(url.to_string());
-    }
-
     for pattern in &patterns {
         let url_inner = url.clone();
         if let Some((param, domain)) = pattern.split_once('@') {
@@ -75,7 +73,7 @@ fn clean_url(text: String, patterns: Vec<String>) -> Result<String, String> {
                     .query_pairs()
                     .filter(|x| !WildMatch::new(param).matches(&x.0));
                 // Replace parameters in URL
-                url.query_pairs_mut().clear().extend_pairs(query);
+                replace_query(&mut url, query);
             }
         } else {
             // Filter parameters to exclude blocked entries
@@ -83,9 +81,23 @@ fn clean_url(text: String, patterns: Vec<String>) -> Result<String, String> {
                 .query_pairs()
                 .filter(|x| !WildMatch::new(pattern).matches(&x.0));
             // Replace parameters in URL
-            url.query_pairs_mut().clear().extend_pairs(query);
+            replace_query(&mut url, query);
         }
     }
 
     Ok(url.to_string())
+}
+
+fn replace_query<'a>(
+    url: &mut Url,
+    query_pairs: impl IntoIterator<Item = (Cow<'a, str>, Cow<'a, str>)>,
+) {
+    url.set_query(None);
+    for (k, v) in query_pairs {
+        if v.is_empty() {
+            url.query_pairs_mut().append_key_only(&k);
+        } else {
+            url.query_pairs_mut().append_pair(&k, &v);
+        }
+    }
 }

src/tests.rs
@@ -1,0 +1,52 @@
+use super::*;
+
+#[cfg(test)]
+use pretty_assertions::assert_eq;
+
+const PATTERNS: [&str; 2] = ["delete_me", "test@example.com"];
+
+#[test]
+fn query_parameters() -> Result<(), String> {
+    for (url, expected) in [
+        ("http://test.org/meow", "http://test.org/meow"),
+        ("http://test.org/page?delete_me", "http://test.org/page"),
+        (
+            "http://test.org/page?delete_me&keep_me",
+            "http://test.org/page?keep_me",
+        ),
+    ] {
+        assert_eq!(
+            clean_url(
+                url.to_owned(),
+                PATTERNS.into_iter().map(|v| v.into()).collect()
+            )?,
+            expected.to_owned()
+        );
+    }
+
+    Ok(())
+}
+
+#[test]
+fn url_fragments() -> Result<(), String> {
+    for (url, expected) in [
+        (
+            "http://test.org/page?delete_me#fragment",
+            "http://test.org/page#fragment",
+        ),
+        (
+            "http://test.org/page#fragment",
+            "http://test.org/page#fragment",
+        ),
+    ] {
+        assert_eq!(
+            clean_url(
+                url.to_owned(),
+                PATTERNS.into_iter().map(|v| v.into()).collect()
+            )?,
+            expected.to_owned()
+        );
+    }
+
+    Ok(())
+}